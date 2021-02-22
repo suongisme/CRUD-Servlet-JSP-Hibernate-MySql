4948f5fba2f4a5e432001a933b06eaef6a4a9297
Imsuong <suongnguyenytm2001@gmail.com>
2021-02-22 18:57:33 +0700

    add logic for ProductDao

--- src/suongnguyen/learn/dao/ProductDao.java
+++ src/suongnguyen/learn/dao/ProductDao.java
@@ -2,19 +2,44 @@ package suongnguyen.learn.dao;
 
 import java.util.List;
 
+import org.hibernate.Transaction;
+
+import org.hibernate.Session;
+
 import suongnguyen.learn.model.Product;
 
 public class ProductDao implements DAO<String, Product>{
 
 	@Override
 	public void submit(Product data) {
-		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			session.saveOrUpdate(data);
+			transaction.commit();
+			session.close();
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
 		
 	}
 
 	@Override
 	public void delete(String id) {
-		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			Product product = session.find(Product.class, id);
+			session.delete(product);
+			transaction.commit();
+			session.close();
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
 		
 	}
 
@@ -26,8 +51,21 @@ public class ProductDao implements DAO<String, Product>{
 
 	@Override
 	public List<Product> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Product> listProduct = null;
+		Session session = sessionFactory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			listProduct = session.createQuery("FROM Product").list();
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
+		
+		
+		
+		return listProduct;
 	}
 
 }
