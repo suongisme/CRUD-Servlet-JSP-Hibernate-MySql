a6420a69d08f4481cb6e11c6410e47584af2c78d
Imsuong <suongnguyenytm2001@gmail.com>
2021-02-22 22:52:19 +0700

    modified ProductController

--- src/suongnguyen/learn/controller/ProductController.java
+++ src/suongnguyen/learn/controller/ProductController.java
@@ -8,16 +8,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/product")
+import suongnguyen.learn.dao.ProductDao;
+import suongnguyen.learn.model.Product;
+
+@WebServlet("/products/*")
 public class ProductController extends HttpServlet{
 	
 	private static final long serialVersionUID = 1L;
+	
+	private ProductDao productDao;
+	
+	@Override
+	public void init() throws ServletException {
+		productDao = new ProductDao();
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String pathInfo = req.getPathInfo();
+
+		if (pathInfo != null) {
+			int id = Integer.parseInt(pathInfo.substring(1).trim());
+			String isDelete = req.getParameter("delete");
+			if ("true".equals(isDelete)) {
+				productDao.delete(id);
+			} 
+		} 
+		
+		req.setAttribute("products", productDao.getAll());
+		req.getRequestDispatcher("/view/Product.jsp").forward(req, resp);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		
+		String id = req.getParameter("ID");
+		String name = req.getParameter("Name");
+		String price = req.getParameter("Price");
+		String desc = req.getParameter("Description");
+		System.out.println(id);
+		Product product = new Product();
+		if (!(id.equals(""))) product.setId(Integer.parseInt(id));
+		product.setName(name);
+		product.setPrice(Integer.parseInt(price));
+		product.setDesc(desc);
+		
+		productDao.submit(product);
+		req.setAttribute("products", productDao.getAll());
+		req.getRequestDispatcher("view/Product.jsp").forward(req, resp);
+	
 	}
 }
